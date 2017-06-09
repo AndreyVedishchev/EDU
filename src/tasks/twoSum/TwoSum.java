76fa1d1502b6e57154310e0863bead99313857d7
vedischevan <avedishchev@list.ru>
2017-06-09 11:24:37 +0400

    09.06 twoSum

--- src/tasks/twoSum/TwoSum.java
+++ src/tasks/twoSum/TwoSum.java
@@ -1,26 +1,23 @@
 package tasks.twoSum;
-/*есть массив с числами, и чило taget, тебе нужно просканировать массив и найти такие два числа, что бы их сумма давала target
-при этом, по условию, всегда есть 1 решение и использовать один и тот же элемент дважды нельзя
-а в качестве ответа функция должно вернуть номера элменетов массива
-т.е. тех элементов, ктороые удовлетворяют условию nums[i] + nums[j] == target
-ответом будет массив {i,j}*/
+
+import java.util.Arrays;
 
 public class TwoSum {
 
     static int [] nums = {2, 7, 11, 15};
     static int target = 9;
 
-
-
     public static void main(String[] args) {
-        twoSum(nums, target);
+        System.out.println(Arrays.toString(twoSum(nums, target)));
     }
+
     public static int[] twoSum(int[] nums, int target) {
         int[] nums1 = new int[2];
             for (int i = 0; i < nums.length; i++) {
                 for (int j = 0; j < nums.length; j++) {
                     if (nums[i] + nums[j] == target) {
-                        System.out.println(nums[i] + " " + nums[j]);
+                        nums1[0] = nums[i];
+                        nums1[1] = nums[j];
                     }
                 }break;
             }
