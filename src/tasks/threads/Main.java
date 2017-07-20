03c3e991e7d7b8e9d5cb8bde8e44e209bf3b97d0
Andrey <avedishchev@list.ru>
2017-07-21 00:16:24 +0300

    21.07 потоки дз

---
+++ src/tasks/threads/Main.java
@@ -0,0 +1,7 @@
+package tasks.threads;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/tasks/threads/StartThread.java
@@ -0,0 +1,8 @@
+package tasks.threads;
+
+public class StartThread extends Thread{
+    public StartThread(String name) {
+        super(name);
+    }
+
+}
