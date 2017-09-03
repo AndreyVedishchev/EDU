a2b968bcd1121b5de7426251f794e2d284f656d3
Andrey <avedishchev@list.ru>
2017-09-04 00:08:58 +0300

    collections hw

---
+++ src/collections/Test.java
@@ -0,0 +1,47 @@
+package collections;
+
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("а", ".-");
+        map.put("б", "-...");
+        map.put("в", ".--");
+        map.put("г", "--.");
+        map.put("д", "-..");
+        map.put("е", ".");
+        map.put("ж", "...-");
+        map.put("з", "--..");
+        map.put("и", "..");
+        map.put("й", ".---");
+        map.put("к", "-.-");
+        map.put("л", ".-..");
+        map.put("м", "--");
+        map.put("н", "-.");
+        map.put("о", "---");
+        map.put("п", ".--.");
+        map.put("р", ".-.");
+        map.put("с", "...");
+        map.put("т", "-");
+        map.put("у", "..-");
+        map.put("ф", "..-.");
+        map.put("х", "....");
+        map.put("ц", "-.-.");
+        map.put("ч", "---.");
+        map.put("ш", "----");
+        map.put("щ", "--.-");
+        map.put("ъ", ".--.-.");
+        map.put("ы", "-.--");
+        map.put("ь", "-..-");
+        map.put("э", "..-..");
+        map.put("ю", "..--");
+        map.put("я", ".-.-");
+
+        System.out.println(args[0]);
+        char []arr = args[0].toCharArray();
+        for (char anArr : arr) {
+            System.out.print(map.get(String.valueOf(anArr)));
+        }
+    }
+}
