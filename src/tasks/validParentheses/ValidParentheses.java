b061539a1513ba16fa1715489997d4ddd0ef3831
vedischevan <avedishchev@list.ru>
2017-10-25 18:58:13 +0400

    25.10 18.57

--- src/tasks/validParentheses/ValidParentheses.java
+++ src/tasks/validParentheses/ValidParentheses.java
@@ -3,15 +3,34 @@ package tasks.validParentheses;
 public class ValidParentheses {
     public static void main(String[] args) {
         ValidParentheses validParentheses = new ValidParentheses();
-        System.out.println(validParentheses.isValid("[](){"));
+        System.out.println(validParentheses.isValid("(("));
     }
     public boolean isValid(String s) {
+        String sub, subarr;
+        StringBuffer stringBuffer = new StringBuffer();
         char []arr = s.toCharArray();
+
         for(int i = 0; i < arr.length; i++) {
-                if (arr.length%2 == 0 && arr[i] == '(' && arr[i + 1] == ')') return true;
-                if (arr.length%2 == 0 && arr[i] == '[' && arr[i + 1] == ']') return true;
-                if (arr.length%2 == 0 && arr[i] == '{' && arr[i + 1] == '}') return true;
+            if (arr.length%2 == 0) {
+                if (arr[i] == '(' && arr[i + 1] == ')') return true;
+                if (arr[i] == '[' && arr[i + 1] == ']') return true;
+                if (arr[i] == '{' && arr[i + 1] == '}') return true;
+            }
         }
+
+        for (int i = arr.length-1; i >= arr.length/2; i--) {
+            if (arr[i] == '(') arr[i] = ')';
+            if (arr[i] == ')') arr[i] = '(';
+            if (arr[i] == '[') arr[i] = ']';
+            if (arr[i] == ']') arr[i] = '[';
+            if (arr[i] == '{') arr[i] = '}';
+            if (arr[i] == '}') arr[i] = '{';
+            stringBuffer.append(arr[i]);
+        }
+
+        subarr = stringBuffer.toString();
+        sub = s.substring(0, arr.length/2);
+        if (sub.equals(subarr))return true;
         return false;
     }
 }
