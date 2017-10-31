7523dd26ca13fbe921c24f216819dc5cd3a1dfc2
Andrey <avedishchev@list.ru>
2017-11-01 00:56:24 +0300

    validParentheses

--- src/tasks/validParentheses/ValidParentheses.java
+++ src/tasks/validParentheses/ValidParentheses.java
@@ -3,34 +3,42 @@ package tasks.validParentheses;
 public class ValidParentheses {
     public static void main(String[] args) {
         ValidParentheses validParentheses = new ValidParentheses();
-        System.out.println(validParentheses.isValid("(("));
+        System.out.println(validParentheses.isValid("}{[]"));
     }
     public boolean isValid(String s) {
         String sub, subarr;
         StringBuffer stringBuffer = new StringBuffer();
         char []arr = s.toCharArray();
 
-        for(int i = 0; i < arr.length; i++) {
-            if (arr.length%2 == 0) {
-                if (arr[i] == '(' && arr[i + 1] == ')') return true;
-                if (arr[i] == '[' && arr[i + 1] == ']') return true;
-                if (arr[i] == '{' && arr[i + 1] == '}') return true;
-            }
-        }
+//        for(int i = 0; i < arr.length; i++) {
+//            if (arr.length%2 == 0) {
+//                if (arr[i] == '(' && arr[i + 1] == ')' |
+//                    arr[i] == '[' && arr[i + 1] == ']' |
+//                    arr[i] == '{' && arr[i + 1] == '}') return true;
+//
+//            }
+//        }
 
-        for (int i = arr.length-1; i >= arr.length/2; i--) {
+        for (int i = 0; i <= 1; i++) {
+            if (arr[i] == '{') arr[i] = '}';
+            if (arr[i] == '}') arr[i] = '{';
             if (arr[i] == '(') arr[i] = ')';
             if (arr[i] == ')') arr[i] = '(';
             if (arr[i] == '[') arr[i] = ']';
             if (arr[i] == ']') arr[i] = '[';
-            if (arr[i] == '{') arr[i] = '}';
-            if (arr[i] == '}') arr[i] = '{';
+
             stringBuffer.append(arr[i]);
+            System.out.println(arr[i] + "  " + i);
         }
 
-        subarr = stringBuffer.toString();
-        sub = s.substring(0, arr.length/2);
-        if (sub.equals(subarr))return true;
+
+        //subarr = stringBuffer.toString();
+        sub = s.substring(arr.length/2, arr.length);
+
+        System.out.println(stringBuffer + " от начала к середине");
+        System.out.println(sub + " от середины до конца");
+
+        if (sub.equals(stringBuffer))return true;
         return false;
     }
 }
