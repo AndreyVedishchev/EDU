a66afbd6e020cf8b4c4f9d71833ccbf2d13d423b
Andrey <avedishchev@list.ru>
2018-10-16 01:35:48 +0300

    16.10

--- src/tasks/connect/Main.java
+++ src/tasks/connect/Main.java
@@ -1,6 +1,8 @@
 package tasks.connect;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -9,12 +11,22 @@ import java.net.URLConnection;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String site = "http://yandex.ru";
-
+        String site = "https://yandex.ru";
         URL url = new URL(site);
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection(); //создаем соединение
+
         int responseCode = urlConnection.getResponseCode();
-        System.out.println(responseCode);
+        System.out.println(responseCode); //получаем код ответа
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                                  /*буферезированый поток*//*преобразование в символы*//*байтовый поток*/
+        StringBuilder result = new StringBuilder();
+        String input;
+        while((input = bufferedReader.readLine()) != null) {
+            result.append(input).append("\n");
+        }
+
+        System.out.println(result);
 
     }
 }
